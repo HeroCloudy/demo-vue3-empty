7b79969967a33a0c21da54d3d77411503cc98216
youyacoder <hero_cloudy@126.com>
2024-01-28 12:13:05 +0800

    clear code & dd sass & reset-css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HelloWorld from '../components/HelloWorld.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="main">
+    <HelloWorld msg="Hello World" />
   </main>
 </template>
+
+<style lang="scss">
+.main {
+  color: red;
+}
+</style>
