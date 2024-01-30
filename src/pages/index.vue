c51f1a2a784d677585196781714032a4663ce024
youyacoder <hero_cloudy@126.com>
2024-01-30 18:30:29 +0800

    feat: iconify

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -16,6 +16,20 @@
     <p>isOutSide: {{ isOutside }}</p>
 
     <hello-world msg="Hello world Component"></hello-world>
+
+    <!-- A basic anchor icon from Phosphor icons -->
+    <div class="i-ph-anchor-simple-thin" />
+    <!-- An orange alarm from Material Design Icons -->
+    <div class="i-mdi-alarm text-orange-400" />
+    <!-- A large Vue logo -->
+    <div class="i-logos-vue text-3xl" />
+    <!-- Sun in light mode, Moon in dark mode, from Carbon -->
+    <button class="i-carbon-sun dark:i-carbon-moon" />
+    <!-- Twemoji of laugh, turns to tear on hovering -->
+    <div class="i-twemoji-grinning-face-with-smiling-eyes hover:i-twemoji-face-with-tears-of-joy" />
+
+    <!-- custom -->
+    <div class="i-ic:baseline-4g-plus-mobiledata w-2em h-2em" style="color: red"></div>
   </div>
 </template>
 
--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,15 @@
 // uno.config.ts
 import { defineConfig } from 'unocss'
-import presetWind from '@unocss/preset-wind'
+import { presetWind, presetIcons } from 'unocss'
 
 export default defineConfig({
-  presets: [presetWind()]
+  presets: [
+    presetWind(),
+    presetIcons({
+      prefix: 'i-',
+      extraProperties: {
+        display: 'inline-block'
+      }
+    })
+  ]
 })
